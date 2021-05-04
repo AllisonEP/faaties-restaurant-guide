5840c20af32ec5b2f6afd5a7585e37d70858febd
Allison Perry <allisonpshortnorth@gmail.com>
2021-05-03 22:23:27 -0400

    added helper method, delete action, app view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,22 @@
 class SessionsController < ApplicationController
 
-    def new
-    end
 
     def create
-    end 
+        user = User.find_by(username: params[:user][:username])
+
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = User.id
+
+            redirect_to user_path(user)
+
+        else 
+        flash[:message] = "How did you get this trip? Try again!"
+        redirect_to "/login"
+        end 
+    end
 
     def destroy
+        session.clear
+        redirect_to root_path
     end 
 end 
