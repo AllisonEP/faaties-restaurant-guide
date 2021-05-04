c1a654266a504ce30db25dc7629d8c61c1ba10e8
Allison Perry <allisonpshortnorth@gmail.com>
2021-05-04 17:45:08 -0400

    Created create, show, destroy actions 4 restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,19 +1,40 @@
 class RestaurantsController < ApplicationController
     def new
+        @restuarant = Restaurant.new
     end 
 
     def create
+        @restaurant = current_user.restaurants.new(restaurant_params)
+        @restaurant = current_volunteer.projects.build(restaurant_params)
+        if @restaurant.save!
+            flash[:notice] = "Restaurant saved!"
+            redirect_to restaurants_path
+        else
+            render :new
+        end   
     end 
 
     def index
     end 
 
     def show
+        @restaurant = Restaurant.find(params[:id])
     end
 
     def edit
     end
 
-    def destroy
+    def destroy;id
+        @restaurant = Restaurant.find_by(params[:id]).destroy
+        if @restuarant.destroy!
+            flash[:notice] = "Okay, its gone!"
+            redirect_to root_path
     end 
+end
+
+private
+
+def restaurant_params
+    params.require(:restaurant).permit(:name, :hours, :long_or_short_layover, :price_point, :discount, :additional_info)
+end
 end
