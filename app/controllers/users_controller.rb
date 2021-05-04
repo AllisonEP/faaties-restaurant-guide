7d3b1b1338c190d749b4a4518a79ce56be585a51
Allison Perry <allisonpshortnorth@gmail.com>
2021-05-03 21:09:30 -0400

    created user show page and show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,15 +4,28 @@ class UsersController < ApplicationController
     end 
 
     def create
-        @user = User.new
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to @user
+        else 
+            render :new
+        end 
     end 
 
     def index
     end 
 
     def show
+        @user = User.find_by_id(params[:id])
     end
 
     def destroy
     end 
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 end
