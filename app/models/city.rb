6f71feba494bd0661911ad3419126744dcc5d356
Allison Perry <allisonpshortnorth@gmail.com>
2021-05-09 19:33:17 -0400

    added validation to cities for name length

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,7 +2,8 @@ class City < ApplicationRecord
     has_many :restaurants
     has_many :users, through: :restaurants
     accepts_nested_attributes_for :restaurants, reject_if: :all_blank
-    validates :name, presence: true, uniqueness: true
+    validates :name, presence: true, length: {minimum: 3, maximum: 25}, uniqueness: true
     validates :state_or_country, length: {minimum: 3, maximum: 25}, allow_blank: true
     validates :airport_code, length: {is: 3}, allow_blank: true
+    validates_associated :restaurants
 end
