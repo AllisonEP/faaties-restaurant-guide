5d5185c9410843467d2ed0dec11d6640f4dfe0f5
Allison Perry <allisonpshortnorth@gmail.com>
2021-05-10 15:20:09 -0400

    dependent destroy conditional added

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,6 @@
 class City < ApplicationRecord
     has_many :restaurants
-    has_many :users, through: :restaurants
+    has_many :users, through: :restaurants, :dependent => :destroy
     accepts_nested_attributes_for :restaurants, reject_if: :all_blank
     validates :name, presence: true, length: {minimum: 3, maximum: 25}, uniqueness: true
     validates :state_or_country, length: {minimum: 3, maximum: 25}, allow_blank: true
