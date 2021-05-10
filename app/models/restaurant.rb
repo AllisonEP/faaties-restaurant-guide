4e3b74132556f16c161b0ae4d2fdaeeab55ec1f9
Allison Perry <allisonpshortnorth@gmail.com>
2021-05-10 15:20:47 -0400

    accepts_nested_attributes readded

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,16 +1,16 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
   belongs_to :city
-  has_many :recommendations
-  #accepts_nested_attributes_for :city, reject_if: :all_blank
+  has_many :recommendations, :dependent => :destroy
+  accepts_nested_attributes_for :city, reject_if: :all_blank
   validates :name, presence: true
   validates :additional_info, length: {minimum: 5, maximum: 200}, allow_blank: true
   # validates :city_id, presence: true
   #validates_associated :city
 
-  # def city_name
-  #   self.try(:city).try(:name)
-  # end
+  def city_name
+    self.try(:city).try(:name)
+  end
 
   def city_attributes=(attributes)
     city = City.find_or_create_by(attributes)
