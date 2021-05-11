373be08a2b773b053752af8b045ce1923bd2d71b
Allison Perry <allisonpshortnorth@gmail.com>
2021-05-11 14:44:45 -0400

    cleaned up controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,55 +5,56 @@ class RestaurantsController < ApplicationController
 
   def new
       if params[:city_id] && !City.exists?(params[:city_id])
-        redirect_to citys_path, alert: "city not found"
+        redirect_to cities_path #
       else
          @restaurant = Restaurant.new(city_id: params[:city_id])
          set_city
-         
       end
   end 
 
+
   def create
-    @restaurant = current_user.restaurants.new(restaurant_params)
-    if @restaurant.save
-      flash[:notice] = "restaurant saved!"
-      redirect_to restaurant_path(@restaurant)
-    else 
-      flash.now[:messages] = @restaurant.errors.full_messages
-      render :new
-    end 
+      @restaurant = current_user.restaurants.new(restaurant_params)
+      if @restaurant.save
+         redirect_to restaurant_path(@restaurant)
+      else 
+         render :new
+      end 
   end
+
 
   def index
       if params[:city_id]
          set_city
             if @city.nil?
-              redirect_to cities_path, alert: "city not found"
+              redirect_to cities_path
             else
               @restaurants = @city.restaurants
             end
-        else
-            @restaurants = Restaurant.all
-        end
+      else
+         @restaurants = Restaurant.all
+      end
     end 
+
 
   def show
       if params[:city_id]
          set_city
          @restaurant = @city.restaurants.find_by_id(params[:id])
             if @restaurant.nil?
-               redirect_to city_restaurants_path(@city), alert: "restaurant not found"
+               redirect_to city_restaurants_path(@city)
             end
       else
          set_restaurant
       end
   end
 
+
     def edit
         if params[:city_id]
            city = City.find_by_id(params[:city_id])
            if city.nil?
-              redirect_to cities_path, alert: "city not found"
+              redirect_to cities_path
            else
               @restaurant = city.restaurants.find_by_id(params[:id])
               redirect_to city_restaurants_path(city), alert: "restaurant not found" if @restaurant.nil?
@@ -63,21 +64,21 @@ class RestaurantsController < ApplicationController
         end
     end
 
+
     def update
-      set_restaurant
+        set_restaurant
         @restaurant.update(restaurant_params)
           if @restaurant.save
              redirect_to @restaurant
           else
-            flash.now[:messages] = @restaurant.errors.full_messages
              render :edit
           end
      end
 
+
     def destroy
          set_restaurant.destroy
           if @restaurant.destroy!
-             flash[:notice] = "Okay, its gone!"
              redirect_to restaurants_path
           else
              render :show
