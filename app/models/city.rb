261acb70c653739621480626dd317c840b8cda92
Allison Perry <allisonpshortnorth@gmail.com>
2021-05-11 16:48:48 -0400

    validator fix to prevent dup 4 caps

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,9 +1,10 @@
 class City < ApplicationRecord
+    before_save { self.name = name.capitalize }
     has_many :restaurants
     has_many :users, through: :restaurants, :dependent => :destroy
     accepts_nested_attributes_for :restaurants, reject_if: :all_blank
-    validates :name, presence: true, length: {minimum: 3, maximum: 25}, uniqueness: true
+    validates :name, presence: true, length: {minimum: 3, maximum: 25}
+    validates_uniqueness_of :name, :case_sensitive => false
     validates :state_or_country, length: {minimum: 3, maximum: 25}, allow_blank: true
     validates :airport_code, length: {is: 3}, allow_blank: true
-    validates_associated :restaurants
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,12 +1,12 @@
 class Restaurant < ApplicationRecord
+
   belongs_to :user
   belongs_to :city
   has_many :recommendations, :dependent => :destroy
   accepts_nested_attributes_for :city, reject_if: :all_blank
   validates :name, presence: true
   validates :additional_info, length: {minimum: 5, maximum: 200}, allow_blank: true
-  # validates :city_id, presence: true
-  #validates_associated :city
+  validates_associated :city
 
   def city_name
     self.try(:city).try(:name)
