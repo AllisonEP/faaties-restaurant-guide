78dfbb1cbc8a08733034c7625ce66bdf24286121
Allison Perry <allisonpshortnorth@gmail.com>
2021-05-11 21:50:10 -0400

    city scope method

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,10 +1,15 @@
 class City < ApplicationRecord
-    before_save { self.name = name.capitalize }
+    before_validation { self.name = name.capitalize }
     has_many :restaurants
     has_many :users, through: :restaurants, :dependent => :destroy
     accepts_nested_attributes_for :restaurants, reject_if: :all_blank
-    validates :name, presence: true, length: {minimum: 3, maximum: 25}
+    #validates :name, presence: true, length: {minimum: 3, maximum: 25}
     validates_uniqueness_of :name, :case_sensitive => false
     validates :state_or_country, length: {minimum: 3, maximum: 25}, allow_blank: true
     validates :airport_code, length: {is: 3}, allow_blank: true
+
+
+  def self.order_by_alphabet
+    self.order(name: :asc)
+  end
 end
