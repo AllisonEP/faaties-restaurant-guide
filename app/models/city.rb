afdefce11c8061ded6d71aa796f80edad1ad9168
Allison Perry <allisonpshortnorth@gmail.com>
2021-05-11 22:32:10 -0400

    cityvalidations

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,7 +3,7 @@ class City < ApplicationRecord
     has_many :restaurants
     has_many :users, through: :restaurants, :dependent => :destroy
     accepts_nested_attributes_for :restaurants, reject_if: :all_blank
-    #validates :name, presence: true, length: {minimum: 3, maximum: 25}
+    validates :name, presence: true, length: {minimum: 3, maximum: 25}
     validates_uniqueness_of :name, :case_sensitive => false
     validates :state_or_country, length: {minimum: 3, maximum: 25}, allow_blank: true
     validates :airport_code, length: {is: 3}, allow_blank: true
