c58130c2eff84825c6b095fce610338236b56ecb
Allison Perry <allisonpshortnorth@gmail.com>
2021-05-18 16:47:57 -0400

    rec model code removed bc useless

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -4,16 +4,4 @@ class Recommendation < ApplicationRecord
   accepts_nested_attributes_for :restaurant, reject_if: :all_blank
   validates :try, length: {minimum: 3, maximum: 55}, presence: true
   validates :avoid, length: {minimum: 3, maximum: 55}, allow_blank: true
-
-  def restaurant_name
-    self.restaurant.id
-  end 
-
-  def restaurant_attributes=(attributes)
-    if attributes["name"] != ""
-      restaurant = Restaurant.find_or_create_by(attributes)
-      self.restaurant = restaurant if restaurant.valid? || self.restaurant
-    end 
-  end
-
 end
