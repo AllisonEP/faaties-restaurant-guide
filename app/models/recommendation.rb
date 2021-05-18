bdecc5635922e086b2bc79aecb3980f7416f80b3
Allison Perry <allisonpshortnorth@gmail.com>
2021-05-18 15:14:28 -0400

    added actions for recs

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -1,11 +1,19 @@
 class Recommendation < ApplicationRecord
   belongs_to :restaurant
   belongs_to :user
+  accepts_nested_attributes_for :restaurant, reject_if: :all_blank
   validates :try, length: {minimum: 3, maximum: 55}, presence: true
   validates :avoid, length: {minimum: 3, maximum: 55}, allow_blank: true
 
   def restaurant_name
-    self.restaurant.name
+    self.restaurant.id
   end 
+
+  def restaurant_attributes=(attributes)
+    if attributes["name"] != ""
+      restaurant = Restaurant.find_or_create_by(attributes)
+      self.restaurant = restaurant if restaurant.valid? || self.restaurant
+    end 
+  end
 
 end
